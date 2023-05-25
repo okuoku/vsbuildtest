744ea272b7d22a625d355f7fc9cff0bc361e6e6c
okuoku <mjt@cltn.org>
2023-05-25 22:52:44 +0900

    Don't generate script

--- build.cmake
+++ build.cmake
@@ -1,16 +1,11 @@
 set(vsroot "C:\\Program Files\\Microsoft Visual Studio\\2022\\Professional")
 set(vcvars64 "${vsroot}\\VC\\Auxiliary\\Build\\vcvars64.bat")
 
-# Generate chain call 
-
-set(chain ${CMAKE_CURRENT_BINARY_DIR}/chain.bat)
-file(WRITE ${chain} "@chcp 65001 > NUL\ncall \"${vcvars64}\"\nmsbuild hello.sln")
-execute_process(COMMAND chmod +x ${chain})
-
 #set(ENV{PATH} "c:\\Windows\\System32;c:\\Windows")
 
 execute_process(
-    COMMAND ${chain}
+    COMMAND cmd /c chcp 65001 &&
+    ${vcvars64} && msbuild hello.sln 
     RESULT_VARIABLE rr
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/vs/build)
 
