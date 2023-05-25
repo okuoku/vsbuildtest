5a41f8bc2227c081b770ea0173f423456580fdb4
okuoku <mjt@cltn.org>
2023-05-25 22:55:31 +0900

    configure script

--- configure.cmake
+++ configure.cmake
@@ -1,13 +1,17 @@
 set(vsroot "C:\\Program Files\\Microsoft Visual Studio\\2022\\Professional")
 set(vcvars64 "${vsroot}\\VC\\Auxiliary\\Build\\vcvars64.bat")
+set(cmakeext_bin "${vsroot}\\Common7\\IDE\\CommonExtensions\\Microsoft\\CMake\\CMake\\bin")
 
 #set(ENV{PATH} "c:\\Windows\\System32;c:\\Windows")
 
 set(builddir "${CMAKE_CURRENT_LIST_DIR}/build")
+file(MAKE_DIRECTORY "${builddir}")
 
 execute_process(
     COMMAND cmd /c chcp 65001 &&
-    ${vcvars64} && msbuild hello.sln 
+    ${vcvars64} && ${cmakeext_bin}\\cmake 
+    -G "Visual Studio 17 2022" 
+    -A x64 ../vs
     RESULT_VARIABLE rr
     WORKING_DIRECTORY ${builddir})
 
